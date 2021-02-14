bbcfd1ac988e1abc5da8dd270e19b3d5d1843068
Leo Ferrianci <58373615+Leo-Ferrianci@users.noreply.github.com>
2021-02-14 11:47:03 -0300

    Inciando Front-end

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {
+  Col,
+  Row,
+  Button,
+  Container,
+} from 'reactstrap';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container style={{ marginTop: "15%", marginLeft: "15%" }}>
+      <Row style={{ marginBottom: "10%" }}>
+        <Col lg="2"className="text-center">
+          <Button color="primary">Janela 1</Button>
+          <p> Ligado </p>
+        </Col>
+        <Col lg="2">
+          <Button color="primary">Janela 2</Button>
+        </Col>
+        <Col lg="2">
+          <Button color="primary">Janela 3</Button>
+        </Col>
+        <Col lg="2">
+          <Button color="primary">Janela 4</Button>
+        </Col>
+        <Col lg="2">
+          <Button color="primary">Janela 5</Button>
+        </Col>
+        <Col lg="2">
+          <Button color="primary">Janela 6</Button>
+        </Col>
+      </Row>
+      <Row>
+        <Col lg="2">
+          <Button color="primary">Janela 7</Button>
+        </Col>
+        <Col lg="2">
+          <Button color="primary">Janela 8</Button>
+        </Col>
+        <Col lg="2">
+          <Button color="primary">Janela 9</Button>
+        </Col>
+        <Col lg="2">
+          <Button color="primary">Janela 10</Button>
+        </Col>
+        <Col lg="2">
+          <Button color="primary">Janela 11</Button>
+        </Col>
+        <Col lg="2">
+          <Button color="primary">Janela 12</Button>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
-export default App;
